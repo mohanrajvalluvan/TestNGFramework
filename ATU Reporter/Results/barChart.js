9882181c02297aab2ae6fb276aad2b70b4d0b8da
MohanrajValluvan <MohanrajValluvan@DESKTOP-SCQOUNC>
2020-06-26 10:29:07 +0530

    Test Case Development and execution completed successfully

--- ATU Reporter/Results/barChart.js
+++ ATU Reporter/Results/barChart.js
@@ -1,9 +1,9 @@
             $(document).ready(function(){
-                var s1 = [1,0,0,0,0,0,0,0,0,1,4];
-                var s2 = [0,1,1,1,1,1,1,0,0,0,0];
-                var s3 = [0,0,0,0,0,0,0,1,1,3,0];
+                var s1 = [0,0,0,0,0,0,0,1,4,4,8];
+                var s2 = [1,1,1,1,1,0,0,0,0,0,0];
+                var s3 = [0,0,0,0,0,1,1,3,0,1,0];
 
-var ticks = [22,23,24,25,26,27,28,29,30,31];    $.jqplot('bar', [s1, s2, s3], {
+var ticks = [24,25,26,27,28,29,30,31,32,33];    $.jqplot('bar', [s1, s2, s3], {
         animate: true,axesDefaults:{min:0,tickInterval: 1},        seriesColors: ["#7BB661", "#E03C31", "#21ABCD"],
         stackSeries: false,
         seriesDefaults: {
